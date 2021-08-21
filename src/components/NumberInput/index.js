577ace75f26e9f1024c80bd1959b5315de004159
RhaynerRS <80178543+RhaynerRS@users.noreply.github.com>
2021-08-21 12:39:29 -0300

    e-comerce react - (0.3.4)

--- src/components/NumberInput/index.js
+++ src/components/NumberInput/index.js
@@ -1,4 +1,5 @@
 import styles from './styles.module.scss';
 export default function NumberInput(props) {
-    return (<input className={styles.numberInput}type="number" min="1" max="10" id="" defaultValue="1" name="product_qty" id="numbere" onChange={props.quanti} />)
+    const qtd= props.quanti
+    return (<input className={styles.numberInput}type="number" min="1" max="10" id="" defaultValue="1" name="product_qty" id="numbere" onChange={qtd} />)
 }
