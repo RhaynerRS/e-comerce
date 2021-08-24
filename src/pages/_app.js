3e181ddcba625bc14395d38845f865e185008258
RhaynerRS <80178543+RhaynerRS@users.noreply.github.com>
2021-08-24 12:21:35 -0300

    e-comerce react - (0.4.1)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -36,6 +36,7 @@ function MyApp({ Component, pageProps }) {
                 <address-fields section="top">
                     <div>
                         <snipcart-label for="cpf">
+                            CPF
                         </snipcart-label>
                         <snipcart-input name="cpf">
                         </snipcart-input>
